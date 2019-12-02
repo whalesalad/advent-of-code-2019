d74e787e51654d7f007b6125523fe16ef436c197
Michael Whalen <michael@whalesalad.com>
2019-12-01 22:16:32 -0800

    Cleaned things up a bit.

--- 02/foo.py
+++ 02/foo.py
@@ -1,12 +1,6 @@
 import sys
 import itertools
 
-commands = {
-    '1': 'add',
-    '2': 'multiply',
-    '99': 'exit'
-}
-
 ADD, MULTIPLY = [1, 2]
 
 
@@ -15,6 +9,10 @@ class TerminateException(Exception):
 
 
 def in_chunks(iterable, n, fillvalue=None):
+    """
+    Borrowed this from another personal project.
+
+    """
     args = [iter(iterable)] * n
     for c in itertools.zip_longest(*args, fillvalue=fillvalue):
         yield filter(None, c)
@@ -76,11 +74,8 @@ class Computer(object):
             while True:
                 self.read()
 
-        except TerminateException as e:
-            print('terminating')
+        except TerminateException:
             pass
-
-        print('state', self.state)
 
         return self.state
 
@@ -96,9 +91,6 @@ def main(data):
 
 if __name__ == '__main__':
     mine = '1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,5,19,23,1,23,6,27,1,5,27,31,1,31,6,35,1,9,35,39,2,10,39,43,1,43,6,47,2,6,47,51,1,5,51,55,1,55,13,59,1,59,10,63,2,10,63,67,1,9,67,71,2,6,71,75,1,5,75,79,2,79,13,83,1,83,5,87,1,87,9,91,1,5,91,95,1,5,95,99,1,99,13,103,1,10,103,107,1,107,9,111,1,6,111,115,2,115,13,119,1,10,119,123,2,123,6,127,1,5,127,131,1,5,131,135,1,135,6,139,2,139,10,143,2,143,9,147,1,147,6,151,1,151,13,155,2,155,9,159,1,6,159,163,1,5,163,167,1,5,167,171,1,10,171,175,1,13,175,179,1,179,2,183,1,9,183,0,99,2,14,0,0'
-
-    # c.state[1] = 12
-    # c.state[2] = 2
 
     for x in range(0, 100):
         for y in range(0, 100):
